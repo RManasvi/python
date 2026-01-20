3cbd10ab7902152ba147b196f29fe1c57abfd6ce
R. Manasvi <92326435+RManasvi@users.noreply.github.com>
2026-01-20 04:11:49 +0000

    20.1.26

---
+++ inbuilt_fun.py
@@ -0,0 +1,25 @@
+### some common build in functions:-
+
+
+# int()
+print(int(2.90))
+# str()
+print(str(5))
+# float()
+print(float(5))
+# input()
+
+# eval()
+
+
+# min() and max()
+
+# abs()-->absolute +ve no.
+
+# type()
+
+# len()
+
+# round()
+
+# range()
